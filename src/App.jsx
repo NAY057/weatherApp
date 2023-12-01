5134f14633733269082f60b319529d52a59172c0
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2023-11-30 21:53:26 -0500

    new settings added for reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,26 @@ const App = () => {
   }
   //ejemplo del objeto action: action {type: "XXX", payload: "XXX"}
   const reducer = (state,action) => {
+    switch (action.type) {
+      case 'SET_ALL_WEATHER':
+        const weatherCity = action.payload
+        const newAllWeather = {...state.allWeather, ...weatherCity}
+        return {...state, allWeather:newAllWeather}
+      case 'SET_CHART_DATA':
+        const chartDataCity = action.payload
+        const newAllChartData = {...state.allChartData, ...chartDataCity}
+        return {...state, allChartData:newAllChartData}
+      case 'SET_FORECAST_ITEM_LIST':
+        const forecastItemListCity = action.payload
+        const newAllForecastItemListCity = {...state.allForecastItemList, ...forecastItemListCity}
+        return {...state, allForecastItemList:newAllForecastItemListCity}
+      default:
+        return state
 
+    }
   }
   // El hook reducer se suele utilizar cuando se manejan estados mas complejos y tambien tiene incorporado la funcionalidad del Hook useMemo
+  //nConcepto Single Source of Truth
   const [state, dispatch] = useReducer(reducer, initialValue)
 
   /*
